1e6eeeb3c1310fbc93c524a73281e1c19a034acb
Gauravi01 <126893240+Gauravi01@users.noreply.github.com>
2024-09-29 17:43:06 +0530

    sideshow

---
+++ school/resources/views/parent/my_student.blade.php
@@ -0,0 +1,81 @@
+@extends('layouts.app')
+@section('content')
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>My Student List</h1>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class ="content">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card ">
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                @include('_message')
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Parent Student List</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body p-0">
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Profile</th>
+                                    <th>Student Name</th>
+                                    <th>Email</th>
+                                    <th>parent Name</th>
+                                    <th>Created Date</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($getRecord as $value)
+                                    <tr>
+                                        <td>{{ $value->id }}</td>
+                                        <td>
+                                            @if (!empty($value->getStudentProfile()))
+                                                <img src = "{{ $value->getStudentProfile() }}"
+                                                    style = "height:50px; width:50px; border-radius:50px;">
+                                            @endif
+                                        </td>
+                                        <td>{{ $value->name }} {{ $value->last_name }}</td>
+                                        <td>{{ $value->email }}</td>
+                                        <td>{{ $value->parent_name }} {{ $value->parent_last_name }}</td>
+                                        <td>{{ date('d-m-Y', strtotime($value->created_at)) }}</td>
+                                        <td>
+                                            <a class="btn btn-primary btn-sm"
+                                                href="{{ url('parent/my_student/subject/' . $value->id) }}">Subject</a>
+                                        </td>
+                                        {{-- 
+                        <td style ="min-width:150px;">
+                           <a href ="{{ url('admin/parent/assign_student_parent_delete/' . $value->id) }}"
+                              class ="btn btn-danger btn-sm">Delete</a>
+                        </td>
+                        --}}
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                        <div style ="padding:10px; float:right;">
+                        </div>
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+            </div>
+            <!-- /.col -->
+    </div>
+    </section>
+@endsection
--- school/routes/web.php
+++ school/routes/web.php
@@ -1,16 +1,16 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\UserController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ClassController;
+use App\Http\Controllers\ClassSubjectController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ParentController;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\SubjectController;
 use App\Http\Controllers\TeacherController;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\ClassSubjectController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,17 +27,16 @@ use App\Http\Controllers\ClassSubjectController;
 //     return view('welcome');
 // });
 
-Route::get('/',[AuthController::class,'login']);
-Route::post('login',[AuthController::class,'AuthLogin']);
-Route::get('logout',[AuthController::class,'logout']);
-Route::get('forgot-password',[AuthController::class,'forgotpassword']);
-Route::post('forgot-password',[AuthController::class,'PostForgotPassword']);
+Route::get('/', [AuthController::class, 'login']);
+Route::post('login', [AuthController::class, 'AuthLogin']);
+Route::get('logout', [AuthController::class, 'logout']);
+Route::get('forgot-password', [AuthController::class, 'forgotpassword']);
+Route::post('forgot-password', [AuthController::class, 'PostForgotPassword']);
 
+Route::group(['middleware' => 'admin'], function () {
 
-Route::group(['middleware' => 'admin'],function(){
-
-    Route::get('admin/dashboard',[DashboardController::class,'dashboard']);
-    Route::get('admin/admin/list',[AdminController::class,'list']);
+    Route::get('admin/dashboard', [DashboardController::class, 'dashboard']);
+    Route::get('admin/admin/list', [AdminController::class, 'list']);
     Route::get('admin/admin/add', [AdminController::class, 'add']);
     Route::post('admin/admin/add', [AdminController::class, 'insert']);
     Route::get('admin/admin/edit/{id}', [AdminController::class, 'edit']);
@@ -45,8 +44,8 @@ Route::group(['middleware' => 'admin'],function(){
     Route::get('admin/admin/delete/{email}', [AdminController::class, 'delete']);
 
     //teacher Routes
-   
-    Route::get('admin/teacher/list',[TeacherController::class,'list']);
+
+    Route::get('admin/teacher/list', [TeacherController::class, 'list']);
     Route::get('admin/teacher/add', [TeacherController::class, 'add']);
     Route::post('admin/teacher/add', [TeacherController::class, 'insert']);
     Route::get('admin/teacher/edit/{id}', [TeacherController::class, 'edit']);
@@ -54,18 +53,18 @@ Route::group(['middleware' => 'admin'],function(){
     Route::get('admin/teacher/delete/{email}', [TeacherController::class, 'delete']);
 
     //student Routes
-    Route::get('admin/student/list',[StudentController::class,'list']);
+    Route::get('admin/student/list', [StudentController::class, 'list']);
     Route::get('admin/student/add', [StudentController::class, 'add']);
-    Route::post('admin/student/add', [StudentController::class, 'insert']); 
+    Route::post('admin/student/add', [StudentController::class, 'insert']);
     Route::get('admin/student/edit/{id}', [StudentController::class, 'edit']);
     Route::post('admin/student/edit/{id}', [StudentController::class, 'update']);
     Route::get('admin/student/delete/{email}', [StudentController::class, 'delete']);
-    
+
     //parent Routes
 
-    Route::get('admin/parent/list',[ParentController::class,'list']);
+    Route::get('admin/parent/list', [ParentController::class, 'list']);
     Route::get('admin/parent/add', [ParentController::class, 'add']);
-    Route::post('admin/parent/add', [ParentController::class, 'insert']); 
+    Route::post('admin/parent/add', [ParentController::class, 'insert']);
     Route::get('admin/parent/edit/{id}', [ParentController::class, 'edit']);
     Route::post('admin/parent/edit/{id}', [ParentController::class, 'update']);
     Route::get('admin/parent/delete/{email}', [ParentController::class, 'delete']);
@@ -74,18 +73,18 @@ Route::group(['middleware' => 'admin'],function(){
     Route::get('admin/parent/assign_student_parent/{student_id}/{parent_id}', [ParentController::class, 'AssignStudentParent']);
     Route::get('admin/parent/assign_student_parent_delete/{student_id}', [ParentController::class, 'AssignStudentParentDelete']);
 
-    //class Routes 
+    //class Routes
 
-    Route::get('admin/class/list',[ClassController::class,'list']);
+    Route::get('admin/class/list', [ClassController::class, 'list']);
     Route::get('admin/class/add', [ClassController::class, 'add']);
     Route::post('admin/class/add', [ClassController::class, 'insert']);
     Route::get('admin/class/edit/{id}', [ClassController::class, 'edit']);
     Route::post('admin/class/edit/{id}', [ClassController::class, 'update']);
     Route::get('admin/class/delete/{id}', [ClassController::class, 'delete']);
 
-    //subject Routes 
+    //subject Routes
 
-    Route::get('admin/subject/list',[SubjectController::class,'list']);
+    Route::get('admin/subject/list', [SubjectController::class, 'list']);
     Route::get('admin/subject/add', [SubjectController::class, 'add']);
     Route::post('admin/subject/add', [SubjectController::class, 'insert']);
     Route::get('admin/subject/edit/{id}', [SubjectController::class, 'edit']);
@@ -94,7 +93,7 @@ Route::group(['middleware' => 'admin'],function(){
 
     //assign-subject
 
-    Route::get('admin/assign-subject/list',[ClassSubjectController::class,'list']);
+    Route::get('admin/assign-subject/list', [ClassSubjectController::class, 'list']);
     Route::get('admin/assign-subject/add', [ClassSubjectController::class, 'add']);
     Route::post('admin/assign-subject/add', [ClassSubjectController::class, 'insert']);
     Route::get('admin/assign-subject/edit/{id}', [ClassSubjectController::class, 'edit']);
@@ -103,54 +102,55 @@ Route::group(['middleware' => 'admin'],function(){
 
     Route::get('admin/assign-subject/edit_single/{id}', [ClassSubjectController::class, 'edit_single']);
     Route::post('admin/assign-subject/edit_single/{id}', [ClassSubjectController::class, 'update_single']);
-   
+
     //my aacount
-    Route::get('admin/account', [AdminController::class,'AdminAccount']);
-    Route::post('admin/account', [AdminController::class,'UpdateAdminAccount']);
+    Route::get('admin/account', [AdminController::class, 'AdminAccount']);
+    Route::post('admin/account', [AdminController::class, 'UpdateAdminAccount']);
     //change password
 
     Route::get('admin/change-password', [UserController::class, 'changePassword']);
     Route::post('admin/change-password', [UserController::class, 'UpdatePassword']);
 
-   
-   
-   
 });
 
-Route::group(['middleware' => 'teacher'],function(){
+Route::group(['middleware' => 'teacher'], function () {
 
-    Route::get('teacher/dashboard',[DashboardController::class,'dashboard']);
+    Route::get('teacher/dashboard', [DashboardController::class, 'dashboard']);
 
     Route::get('teacher/change-password', [UserController::class, 'changePassword']);
     Route::post('teacher/change-password', [UserController::class, 'UpdatePassword']);
 
-
-    Route::get('teacher/account', [TeacherController::class,'TeacherAccount']);
-    Route::post('teacher/account', [TeacherController::class,'UpdateTeacherAccount']);
-
-
+    Route::get('teacher/account', [TeacherController::class, 'TeacherAccount']);
+    Route::post('teacher/account', [TeacherController::class, 'UpdateTeacherAccount']);
 
 });
 
-Route::group(['middleware' => 'student'],function(){
+Route::group(['middleware' => 'student'], function () {
 
-    Route::get('student/dashboard',[DashboardController::class,'dashboard']);
+    Route::get('student/dashboard', [DashboardController::class, 'dashboard']);
 
     Route::get('student/change-password', [UserController::class, 'changePassword']);
     Route::post('student/change-password', [UserController::class, 'UpdatePassword']);
-  
-    Route::get('student/account', [StudentController::class,'studentAccount']);
-    Route::post('student/account', [StudentController::class,'UpdateStudentAccount']);
+
+    Route::get('student/my_subject', [SubjectController::class, 'MySubject']);
+
+    Route::get('student/account', [StudentController::class, 'studentAccount']);
+    Route::post('student/account', [StudentController::class, 'UpdateStudentAccount']);
+
 });
 
-Route::group(['middleware' => 'parent'],function(){
+Route::group(['middleware' => 'parent'], function () {
 
-    Route::get('parent/dashboard',[DashboardController::class,'dashboard']);
+    Route::get('parent/dashboard', [DashboardController::class, 'dashboard']);
 
     Route::get('parent/change-password', [UserController::class, 'changePassword']);
     Route::post('parent/change-password', [UserController::class, 'UpdatePassword']);
 
-    Route::get('parent/account', [ParentController::class,'parentAccount']);
-    Route::post('parent/account', [ParentController::class,'UpdateParentAccount']);
+    Route::get('parent/account', [ParentController::class, 'parentAccount']);
+    Route::post('parent/account', [ParentController::class, 'UpdateParentAccount']);
+
+    Route::get('parent/my_student/subject/{student_id}', [SubjectController::class, 'ParentStudentSubject']);
+
+    Route::get('parent/my_student', [ParentController::class, 'myStudentParent']);
 
 });
